7a48d3fb17efc71592a5acfe65ed1958ff44dbf2
yandreychenko <yandreychenko@ozon.ru>
2024-07-03 18:34:55 +0300

    2

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,40 @@
+﻿using Animal1;
+using System;
+
+namespace Lesson1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                //int num1 = int.Parse(args[0]);
+                //int num2 = int.Parse(args[1]);
+
+                //int sum = num1 + num2;
+                //Console.WriteLine($" {num1} + {num2} = {sum}");
+
+                //foreach (string item in args)
+                //{
+                //    Console.Write($"{item} ,");
+                //}
+                //Console.WriteLine(string.Join(", ", args));
+
+                int sum = 0;
+                int [] array = Array.ConvertAll(args,int.Parse);
+                //foreach (string arg in args)
+                //{
+                //    sum += int.Parse(arg);
+                //}
+                
+                int avg = array.Sum() / args.Length;
+                Console.WriteLine(avg);
+            }
+            else
+            {
+                Console.WriteLine("Need args");
+            }
+        }
+    }
+}
