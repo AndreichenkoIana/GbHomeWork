d10de0876502b733c1889184be4e644fa8c3603d
yandreychenko <yandreychenko@ozon.ru>
2024-07-03 18:35:13 +0300

    3

---
+++ Gb_Education/Gb_Education/FileName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gb_Education
+{
+    internal class FileName
+    {
+        static void Main ()
+        {
+            Console.WriteLine("Введите свое имя: ");
+            var name = Console.ReadLine();
+            var message = $"Привет, уважаемая {name}";
+            Console.WriteLine(message);
+        }
+    }
+}
