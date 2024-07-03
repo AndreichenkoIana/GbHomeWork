b7de16a8a2ca2e5246767bb18ee582dcc94eb4b4
yandreychenko <yandreychenko@ozon.ru>
2024-07-03 18:34:26 +0300

    1

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeWork;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 3)
+        {
+            int result = 0;
+            int num1 = int.Parse(args[0]);
+            string operation = args[1];
+            int num2 = int.Parse(args[2]);
+            if (operation == "+")
+            {
+                result = num1 + num2;
+            }
+            if (operation == "-")
+            {
+                result = num1 - num2;
+            }
+            if (operation == "/")
+            {
+                result = num1 / num2;
+            }
+            if (operation == "*")
+            {
+                result = num1 * num2;
+            }
+            Console.WriteLine($" {num1} {operation} {num2} = {result} ");
+        }
+        else
+            Console.WriteLine("Для использования программы введите: число знак(+-/*) число");
+
+    }
+}
