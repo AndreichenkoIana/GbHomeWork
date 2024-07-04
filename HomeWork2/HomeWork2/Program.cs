d41e67213c5eef81378be46be38c80c20a251735
yandreychenko <yandreychenko@ozon.ru>
2024-07-04 12:14:04 +0300

    HomeWork2

---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HomeWork;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] a = {
+                     { 7, 3, 2 },
+                     { 4, 9, 6 },
+                     { 1, 8, 5 }
+                   };
+        int y = a.GetLength(0);
+        int x = a.GetLength(1);
+        int[] sort = new int[x * y];
+        int IndexSort = 0;
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                sort[IndexSort] = a[i, j];
+                IndexSort++;
+            }
+        }
+        Array.Sort(sort);
+        IndexSort = 0;
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                a[i, j] = sort[IndexSort];
+                IndexSort++;
+                Console.Write($"{a[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+    
